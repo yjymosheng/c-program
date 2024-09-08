d591beb6295b691c238c3830a22e631346479f49
yjymosheng <jinyu@isrc.iscas.ac.cn>
2024-09-08 21:55:18 +0800

    update

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BRANCH = $( git branch | grep "*" | awk '{print $2}')
-echo "$(BRANCH)"
+BRANCH=$( git branch | grep "*" | awk '{print $2}')
+REPO=$(git remote)
 
-cd /home/mosheng/c-program
+cd /home/mosheng/github/c-program
 
 git add .
 git commit -m update
-# git push
+git push $REPO $BRANCH

e3cf4cb343ed8b8b3bf2825b3ad7fdc42a6eac9e
yjymosheng <jinyu@isrc.iscas.ac.cn>
2024-09-12 09:59:51 +0800

    update

--- calculate/calculate.c
+++ calculate/calculate.c
@@ -16,7 +16,7 @@ int sub(char* a , char* b){
 
 int multiply(char* a , char* b){
 
-    printf("\tfrom mut function,a: %d\t b: %d\n",*a,*b);
+    // printf("\tfrom mut function,a: %d\t b: %d\n",*a,*b);
     return (*a)*(*b);
 }
 
--- main.c
+++ main.c
@@ -5,11 +5,22 @@
 #include "struct/stack/stack.h"
 #include <stdio.h>
 
+
+/*测试案例
+ *12-
+ * 计算方式， 1-2
+ *12-45+*
+ *计算方式  （1-2）*（4+5）
+ */
+
 int main() {
   Container *input = init_container(); // 用一个结构体来存放输入的表达是
   Stack *stack = init_stack();
   greeting(); // 程序运行时的输出
-  while (input->get_input(input)) {
+  int i=0;
+  // while (input->get_input(input)) {
+  while (i++<3&& input->get_input(input) ) {
+
     while (!(input->is_empty(input))) {
       printf_contain(input);
       switch (analyze(input)) {
@@ -29,5 +40,6 @@ int main() {
   }
 
   release_container(input);
+  release_stack(stack);
   return 0;
 }
